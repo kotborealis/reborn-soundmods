111a3f4bc65195d7669d2f29f71318562f1f3657
kotborealis <kotborealis@gmail.com>
2018-03-20 12:54:04 +0300

    initial

---
+++ src/com/lc/Main.java
@@ -0,0 +1,77 @@
+package com.lc;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        create_dirs();
+        resourcecompiler("D:\\SteamLibrary\\steamapps\\common\\dota 2 beta\\content\\soundmods_content\\*");
+        gameinfo();
+        create_vpk();
+    }
+
+    private static String dota2_path(){
+        // TODO
+        return "D:\\SteamLibrary\\steamapps\\common\\dota 2 beta";
+    }
+
+    static private String game_path = Paths.get(dota2_path(), "game").toString();
+    static private String content_path = Paths.get(dota2_path(), "content").toString();
+
+    private static void create_dirs(){
+        Path[] paths = {
+            Paths.get(content_path, "soundmods_content"),
+            Paths.get(game_path, "soundmods")
+        };
+
+        for(Path p : paths){
+            System.out.println("Creating dir " + p.toString());
+            File f = new File(p.toString());
+            if(!f.exists()) {
+                f.mkdir();
+            }
+        }
+    }
+
+    private static void resourcecompiler(String input) throws IOException, InterruptedException {
+        String bin = Paths.get(game_path, "bin", "win64", "resourcecompiler.exe").toString();
+        Process process = Runtime.getRuntime().exec(
+                "\"" + bin + "\"" +
+                        " -i " + "\"" + input + "\"" +
+                        " -r "
+        );
+        process.waitFor();
+    }
+
+    private static void gameinfo() throws IOException {
+        Path path = Paths.get(game_path, "dota", "gameinfo.gi");
+        Charset charset = StandardCharsets.UTF_8;
+        String content = new String(Files.readAllBytes(path), charset);
+        content = content.replace(
+                "\t\t\tGame\t\t\t\tdota",
+                "\t\t\tGame\t\t\t\tsoundmods\n\t\t\tGame\t\t\t\tdota"
+        );
+        Files.write(path, content.getBytes(charset));
+    }
+
+    private static void create_vpk() throws IOException, InterruptedException {
+        String input = Paths.get(game_path, "soundmods_content").toString();
+        String output_vpk = Paths.get(game_path, "soundmods_content.vpk").toString();
+        String output = Paths.get(game_path, "soundmods", "pak01_000.vpk").toString();
+
+        String bin = Paths.get("./", "vpk.exe").toString();
+        Process process = Runtime.getRuntime().exec(
+                "\"" + bin + "\"" + " " + "\"" + input + "\""
+        );
+        process.waitFor();
+
+        new File(output_vpk).renameTo(new File(output));
+    }
+}
