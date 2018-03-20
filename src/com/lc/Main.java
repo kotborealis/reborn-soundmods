b432cceea0564cf69f59c8103e140e4df862fec2
kotborealis <kotborealis@gmail.com>
2018-03-20 13:11:10 +0300

    Refactoring

--- src/com/lc/Main.java
+++ src/com/lc/Main.java
@@ -2,6 +2,8 @@ package com.lc;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -12,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         create_dirs();
-        resourcecompiler("D:\\SteamLibrary\\steamapps\\common\\dota 2 beta\\content\\soundmods_content\\*");
+        resourcecompiler();
         gameinfo();
         create_vpk();
     }
@@ -40,13 +42,24 @@ public class Main {
         }
     }
 
-    private static void resourcecompiler(String input) throws IOException, InterruptedException {
+    private static void copy(InputStream in, OutputStream out) throws IOException {
+        while (true) {
+            int c = in.read();
+            if (c == -1) break;
+            out.write((char)c);
+        }
+    }
+
+    private static void resourcecompiler() throws IOException, InterruptedException {
+        String input = Paths.get(content_path, "soundmods_content").toString() + File.separator + "*";
+        System.out.println(input);
         String bin = Paths.get(game_path, "bin", "win64", "resourcecompiler.exe").toString();
         Process process = Runtime.getRuntime().exec(
                 "\"" + bin + "\"" +
                         " -i " + "\"" + input + "\"" +
                         " -r "
         );
+        copy(process.getInputStream(), System.out);
         process.waitFor();
     }
 
@@ -70,6 +83,7 @@ public class Main {
         Process process = Runtime.getRuntime().exec(
                 "\"" + bin + "\"" + " " + "\"" + input + "\""
         );
+        copy(process.getInputStream(), System.out);
         process.waitFor();
 
         new File(output_vpk).renameTo(new File(output));
